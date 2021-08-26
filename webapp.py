4927a9a194a626b671ee8a2212da9bb318707b28
Claude Manchester <cloudeal@hotmail.com>
2021-08-26 22:21:20 +0800

    Add registration handler

--- webapp.py
+++ webapp.py
@@ -1,4 +1,7 @@
+import os
 import json
+import time
+import datetime
 
 from typing import Any
 from tornado import httputil
@@ -68,8 +71,13 @@ class HeartbeatHandler(BaseHandler):
     Get IOT device status
     """
     def get(self):
+        device_sn = self.get_argument("sn")
         with open("./hearbeat.log", "a") as fd:
-            fd.write(f"{self.request.remote_ip}\n")
+            fd.write(
+                f"{str(datetime.datetime.now())[:19]}"
+                f"  {device_sn}"
+                f"  {self.request.remote_ip}"
+                f"\n")
         return self.write("ok")
 
 
@@ -88,6 +96,65 @@ class DeviceDataHandler(BaseHandler):
     pass
 
 
+class RegistrationHandler(BaseHandler):
+    """
+    Registration of daily task,
+    such as reading, playing, watching etc.
+    """
+    def get(self):
+        # Preset dict
+        OP_DICT = {
+            "01": "Relax Alone",
+            "11": "Relax Together",
+            "12": "Hard Entertainment",
+            "13": "Study",
+            "14": "Wash",
+            "15": "Before Sleep Relax",
+            "16": "Sleep"
+        }
+
+        ROLE_DICT = {
+            "0": "Pretty",
+            "1": "Handsome"
+        }
+
+        STATUS_DICT = {
+            "0": "Start",
+            "1": "End",
+            "2": "Pause",
+            "3": "Resume"
+        }
+
+        EXPECT_LEN = 4
+
+        # Parameters check
+        seq = self.get_argument("seq")
+        if len(seq) != EXPECT_LEN:
+            return self.write("数据有误！")
+
+        op = OP_DICT.get(seq[0] + seq[1])
+        role = ROLE_DICT.get(seq[2])
+        status = STATUS_DICT.get(seq[3])
+
+        if op is None or role is None or status is None:
+            return self.write("参数阈值出错！")
+
+        # Create log dir
+        LOG_PATH = "./.log/registration/"
+        if not os.path.exists(LOG_PATH):
+            os.makedirs(LOG_PATH)
+
+        # Record log
+        ts = int(time.time())
+        date = str(datetime.datetime.today())[:10]
+        with open(rf"./.log/registration/{date}.log", "a") as fd:
+            fd.write(f"{ts} {op} {role} {status}")
+        return self.write("打卡成功")
+
+    def post(self):
+        pass
+
+
 def make_app():
     return tornado.web.Application([
         (r"/health", HealthHandler),
@@ -96,6 +163,7 @@ def make_app():
         (r"/heartbeat", HeartbeatHandler),
         (r"/task", TaskDistributionHandler),
         (r"/get_server_ip", ServerIPHandler),
+        (r"/registration", RegistrationHandler),
     ])
 
 
