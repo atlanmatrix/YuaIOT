850b72945e80fbebe4010bc3dc620a61c15eb0a1
Claude Manchester <cloudeal@hotmail.com>
2021-09-08 15:03:06 +0800

    update utils

--- utils.py
+++ utils.py
@@ -1,2 +1,31 @@
+import os
+
+
+def random_hex_str(len):
+    hex_str = ''.join(
+        [('0'+hex(ord(os.urandom(1)))[2:])[-2:] for _ in range(len // 2)])
+    return hex_str
+
+
+def get_file_size(file_path):
+    if os.path.exists(file_path):
+        return os.path.getsize(file_path)
+    else:
+        return 0
+
+
+def get_entire_html(block):
+    tpml = """
+        <!DOCTYPE html>
+        <html>
+            <head> <title>ESP32 Web Server</title> </head>
+            <body>$$</body>
+        </html>
+    """
+
+    html = tpml.replace('$$', block)
+    return html
+
+
 def init_config():
     pass
